61e7721e75b8f1bc56e3fa99db632f699a10da68
KWilkerson <kevin.j.wilkerson@gmail.com>
2022-11-10 13:19:47 -0600

    added basic footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section>
+      <div className="footer flex-row">
+        <p>&copy; 2022 Neurosurgical Associates, LLC. All rights reserved.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
